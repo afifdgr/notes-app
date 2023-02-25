c54b50cd83df56b57672292914afd0e01a354754
afifdgr <afifdgr3@gmail.com>
2023-02-25 08:46:04 +0700

    # Argument Parsing with Yargs

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new Note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("title: " + argv.title);
+    console.log("body: " + argv.body);
+  },
+});
 
-if (command === "add") {
-  console.log("note");
-} else if (command === "remove") {
-  console.log("removing note");
-}
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a Note",
+  handler: function () {
+    console.log("removing the note");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List Notes",
+  handler: function () {
+    console.log("List Notes");
+  },
+});
+
+// Create Read command
+yargs.command({
+  command: "read",
+  describe: "Read a Note",
+  handler: function () {
+    console.log("Read a Note");
+  },
+});
+
+console.log(yargs.argv);
